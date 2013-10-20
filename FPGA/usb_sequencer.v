677aa823ec37a36cbaaa5f92f3319dbb57fdbfb6
Chad Harrington <chad@deercreeklabs.com>
2013-10-19 20:46:53 -0600

    USB controller WIP

---
+++ FPGA/usb_sequencer.v
@@ -0,0 +1,18 @@
+module usb_sequencer
+  (
+   input  clk,
+   input  reset_n,
+   input  rxf_n,
+   input  txe_n,
+   output rd_n,
+   output wr_n, 
+   output nibble_write_enable,
+   output chunk_write_enable
+   );
+
+endmodule // usb_sequencer
+
+   
+   
+
+   
