39ac9dd2a60eaac99d47c6d465e37a01dd2fd310
Chad Harrington <chad@deercreeklabs.com>
2013-10-29 13:44:21 -0600

    Fixed usb_sequencer bit ordering problem. Defined outputs during reset.

--- FPGA/usb_sequencer.v
+++ FPGA/usb_sequencer.v
@@ -18,10 +18,10 @@ module usb_sequencer
    reg [4:0]        state, next_state;
 
    assign state_out = state;
-   assign rd_n = output_bits[0];
-   assign wr_n = output_bits[1];
-   assign data_out_enable = output_bits[2];
-   assign command_write_enable = output_bits[3];
+   assign rd_n = output_bits[3];
+   assign wr_n = output_bits[2];
+   assign data_out_enable = output_bits[1];
+   assign command_write_enable = output_bits[0];
    
    
    // Labeled states
@@ -82,137 +82,143 @@ module usb_sequencer
 
    // Output logic
    always @(*)
-     case (state)
-       0: // start_read
-         begin
-            data_out = 8'hzz;
-            output_bits = 4'b1100;
-         end
-       1: // 
-         begin
-            data_out = 8'hzz;
-            output_bits = 4'b0100;
-         end
-       2: // 
-         begin
-            data_out = 8'hzz;
-            output_bits = 4'b0101;
-         end
-       3: // end_read
-         begin
-            data_out = 8'hzz;
-            output_bits = 4'b1100;
-         end
-       4: // start_write
-         begin
-            data_out = 8'hzz;
-            output_bits = 4'b1100;
-         end
-       5: // 
-         begin
-            data_out = {4'h1, panel_switches[3:0]};
-            output_bits = 4'b1110;
-         end
-       6: // 
-         begin
-            data_out = {4'h1, panel_switches[3:0]};
-            output_bits = 4'b1010;
-         end
-       7: // 
-         begin
-            data_out = {4'h1, panel_switches[3:0]};
-            output_bits = 4'b1010;
-         end
-       8: // 
-         begin
-            data_out = {4'h1, panel_switches[3:0]};
-            output_bits = 4'b1110;
-         end
-       9: // write_p1
-         begin
-            data_out = {4'h1, panel_switches[3:0]};
-            output_bits = 4'b1110;
-         end
-       10: // 
-         begin
-            data_out = {4'h2, panel_switches[7:4]};
-            output_bits = 4'b1110;
-         end
-       11: // 
-         begin
-            data_out = {4'h2, panel_switches[7:4]};
-            output_bits = 4'b1010;
-         end
-       12: // 
-         begin
-            data_out = {4'h2, panel_switches[7:4]};
-            output_bits = 4'b1010;
-         end
-       13: // 
-         begin
-            data_out = {4'h2, panel_switches[7:4]};
-            output_bits = 4'b1110;
-         end
-       14: // write_p2
-         begin
-            data_out = {4'h2, panel_switches[7:4]};
-            output_bits = 4'b1110;
-         end
-       15: // 
-         begin
-            data_out = {4'h3, panel_switches[11:8]};
-            output_bits = 4'b1110;
-         end
-       16: // 
-         begin
-            data_out = {4'h3, panel_switches[11:8]};
-            output_bits = 4'b1010;
-         end
-       17: // 
-         begin
-            data_out = {4'h3, panel_switches[11:8]};
-            output_bits = 4'b1010;
-         end
-       18: // 
-         begin
-            data_out = {4'h3, panel_switches[11:8]};
-            output_bits = 4'b1110;
-         end
-       19: // write_p3
-         begin
-            data_out = {4'h3, panel_switches[11:8]};
-            output_bits = 4'b1110;
-         end
-       20: // 
-         begin
-            data_out = {4'h4, panel_switches[15:12]};
-            output_bits = 4'b1110;
-         end
-       21: // 
-         begin
-            data_out = {4'h4, panel_switches[15:12]};
-            output_bits = 4'b1010;
-         end
-       22: // 
-         begin
-            data_out = {4'h4, panel_switches[15:12]};
-            output_bits = 4'b1010;
-         end
-       23: // 
-         begin
-            data_out = {4'h4, panel_switches[15:12]};
-            output_bits = 4'b1110;
-         end
-       24: // end_write
-         begin
-            data_out = {4'h4, panel_switches[15:12]};
-            output_bits = 4'b1110;
-         end
-       default: // default
-         begin
-            data_out = 8'hzz;
-            output_bits = 4'b1100;
-         end
-     endcase // case (state)
+     if (!reset_n)
+       begin
+          data_out = 8'hzz;
+          output_bits = 4'b1100;
+       end
+     else
+       case (state)
+         0: // start_read
+           begin
+              data_out = 8'hzz;
+              output_bits = 4'b1100;
+           end
+         1: // 
+           begin
+              data_out = 8'hzz;
+              output_bits = 4'b0100;
+           end
+         2: // 
+           begin
+              data_out = 8'hzz;
+              output_bits = 4'b0101;
+           end
+         3: // end_read
+           begin
+              data_out = 8'hzz;
+              output_bits = 4'b1100;
+           end
+         4: // start_write
+           begin
+              data_out = 8'hzz;
+              output_bits = 4'b1100;
+           end
+         5: // 
+           begin
+              data_out = {4'h1, panel_switches[3:0]};
+              output_bits = 4'b1110;
+           end
+         6: // 
+           begin
+              data_out = {4'h1, panel_switches[3:0]};
+              output_bits = 4'b1010;
+           end
+         7: // 
+           begin
+              data_out = {4'h1, panel_switches[3:0]};
+              output_bits = 4'b1010;
+           end
+         8: // 
+           begin
+              data_out = {4'h1, panel_switches[3:0]};
+              output_bits = 4'b1110;
+           end
+         9: // write_p1
+           begin
+              data_out = {4'h1, panel_switches[3:0]};
+              output_bits = 4'b1110;
+           end
+         10: // 
+           begin
+              data_out = {4'h2, panel_switches[7:4]};
+              output_bits = 4'b1110;
+           end
+         11: // 
+           begin
+              data_out = {4'h2, panel_switches[7:4]};
+              output_bits = 4'b1010;
+           end
+         12: // 
+           begin
+              data_out = {4'h2, panel_switches[7:4]};
+              output_bits = 4'b1010;
+           end
+         13: // 
+           begin
+              data_out = {4'h2, panel_switches[7:4]};
+              output_bits = 4'b1110;
+           end
+         14: // write_p2
+           begin
+              data_out = {4'h2, panel_switches[7:4]};
+              output_bits = 4'b1110;
+           end
+         15: // 
+           begin
+              data_out = {4'h3, panel_switches[11:8]};
+              output_bits = 4'b1110;
+           end
+         16: // 
+           begin
+              data_out = {4'h3, panel_switches[11:8]};
+              output_bits = 4'b1010;
+           end
+         17: // 
+           begin
+              data_out = {4'h3, panel_switches[11:8]};
+              output_bits = 4'b1010;
+           end
+         18: // 
+           begin
+              data_out = {4'h3, panel_switches[11:8]};
+              output_bits = 4'b1110;
+           end
+         19: // write_p3
+           begin
+              data_out = {4'h3, panel_switches[11:8]};
+              output_bits = 4'b1110;
+           end
+         20: // 
+           begin
+              data_out = {4'h4, panel_switches[15:12]};
+              output_bits = 4'b1110;
+           end
+         21: // 
+           begin
+              data_out = {4'h4, panel_switches[15:12]};
+              output_bits = 4'b1010;
+           end
+         22: // 
+           begin
+              data_out = {4'h4, panel_switches[15:12]};
+              output_bits = 4'b1010;
+           end
+         23: // 
+           begin
+              data_out = {4'h4, panel_switches[15:12]};
+              output_bits = 4'b1110;
+           end
+         24: // end_write
+           begin
+              data_out = {4'h4, panel_switches[15:12]};
+              output_bits = 4'b1110;
+           end
+         default: // default
+           begin
+              data_out = 8'hzz;
+              output_bits = 4'b1100;
+           end
+       endcase // case (state)
 
 endmodule // usb_sequencer
