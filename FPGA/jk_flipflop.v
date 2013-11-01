20b73c60582f0d376c99cf8e8204f0b14f6cc28d
Chad Harrington <chad@deercreeklabs.com>
2013-11-01 15:10:18 -0600

    added jk flipflop

---
+++ FPGA/jk_flipflop.v
@@ -0,0 +1,16 @@
+module jk_flipflop
+  (
+   input clk,
+   input reset_n,
+   input j,
+   input k
+   output q
+   );
+
+   always @(posedge clk, negedge reset_n)
+     if (!reset_n)
+       q <= 1'b0;
+     else
+       q <= (j & ~q) | (~k & q);
+
+endmodule // jk_flipflop
