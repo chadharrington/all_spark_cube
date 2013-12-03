b1ada20cab80105a6d70b5ea276d0cdcfd2a8d6d
Chad Harrington <chad@deercreeklabs.com>
2013-12-03 13:32:56 -0700

    Fixed python_client demo

--- Software/clients/python_client/src/client_demo.py
+++ Software/clients/python_client/src/client_demo.py
@@ -2,14 +2,14 @@
 
 from random import randint
 
-from all_spark_cube_client import AllSparkCubeClient
+from all_spark_cube_client import CubeClient
 
 
-HOST='localhost'
+HOST='192.168.0.100'
 PORT=12345
 
 index = 0
 data = [randint(0, 255) for x in range(4096 * 3)]
 
-client = AllSparkCubeClient(HOST, PORT)
+client = CubeClient(HOST, PORT)
 client.set_data(index, data)
