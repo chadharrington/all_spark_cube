e2ed1e06357cf669c541813eb3a47519b6b4edde
Chad Harrington <chad@deercreeklabs.com>
2013-12-19 23:38:04 -0700

    Added initial stub for cpp cube_server

---
+++ software/server/cube_server.cpp
@@ -0,0 +1,40 @@
+#include "CubeInterface.h"
+
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+
+using namespace ::apache::thrift;
+using namespace ::apache::thrift::protocol;
+using namespace ::apache::thrift::transport;
+using namespace ::apache::thrift::server;
+
+using boost::shared_ptr;
+
+class CubeInterfaceHandler : virtual public CubeInterfaceIf {
+ public:
+  CubeInterfaceHandler() {
+    // Your initialization goes here
+  }
+
+  void set_data(const int16_t index, const std::vector<int16_t> & data) {
+    // Your implementation goes here
+    printf("set_data\n");
+  }
+
+};
+
+int main(int argc, char **argv) {
+  int port = 9090;
+  shared_ptr<CubeInterfaceHandler> handler(new CubeInterfaceHandler());
+  shared_ptr<TProcessor> processor(new CubeInterfaceProcessor(handler));
+  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+
+  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+  server.serve();
+  return 0;
+}
+
